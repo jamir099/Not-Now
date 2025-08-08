6406ce58ef36fc9dd64a2545e50d0c021891fc60
Jamir_Shikalgar <151499529+jamir099@users.noreply.github.com>
2025-08-08 18:41:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import { create, router as _router, defaults } from "json-server";
-const server = create();
-const router = _router("db.json"); 
-const middlewares = defaults();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json"); // path to your db.json file
+const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
